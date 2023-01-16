a66d84547ec8140c3bba5cb0401c368d15af024a
dcolussi <dcolussi.sec@gmail.com>
2023-01-15 21:53:33 -0300

    x

--- METRICAS_PBI/PROCESS/EventHistorical.ps1
+++ METRICAS_PBI/PROCESS/EventHistorical.ps1
@@ -7,15 +7,19 @@ $eventosFiles = ( Get-ChildItem -Path .\DATAEXPORT\EVENTS\ ) | Select-Object -Pr
 
 $result_ev = @()
 foreach($evFile in $eventosFiles){  
+    
     $file = ".\DATAEXPORT\EVENTS\"+$evFile.name
     write-host $evFile
-    $result = @{
-        "data"= Get-Content -Path $file -Raw | ConvertFrom-Json ;
-        "file" = $evFile.name
-    }
+    
+    $result = @{}
+    $result.data = @()
+    $result.data += Get-Content -Path $file -Raw | ConvertFrom-Json ;
+    $result.file = $evFile.name
+    # Append del objeto resultado: Archivo + Data
     $result_ev += $result
 }
 
+# guardar el consolidado en archivo
 ( $result_ev | ConvertTo-Json -Depth 8 )>".\DATAEXPORT\fc_PowerBIEvents.json"
 
 
