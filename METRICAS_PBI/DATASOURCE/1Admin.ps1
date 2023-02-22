f37727f4b7d84c98eb1f686d449a5e12b541076c
COLUSSI Diego M. TENARIS <DCOLUSSI@tenaris.com>
2023-02-22 11:49:48 -0300

    modificacion Datasource

--- METRICAS_PBI/DATASOURCE/1Admin.ps1
+++ METRICAS_PBI/DATASOURCE/1Admin.ps1
@@ -9,13 +9,13 @@ $file = "./DATAEXPORT/md_PowerBIWorkspaceEncryptionStatus.json"
 #>
 
 # Exportar Eventos
-$f_inicio = (Get-Date -Format "yyyy-MM-dd")+"T00:01:00"
+$f_inicio = (Get-Date -Format "yyyy-MM-dd")+"T00:30:00"
 $f_fin= (Get-Date -Format "yyyy-MM-dd")+"T23:59:00"
 $fech = (Get-Date -Format "yyyyMMdd")
 
-$f_inicio = "2023-01-25T00:01:00"
-$f_fin= "2023-01-25T23:59:00"
-$fech = "20230125"
+#$f_inicio = "2023-02-17T00:30:00"
+#$f_fin= "2023-02-17T23:59:00"
+#$fech = "20230217"
 
 $events = Get-PowerBIActivityEvent -StartDateTime $f_inicio -EndDateTime $f_fin
 $events = ConvertFrom-Json $events
--- METRICAS_PBI/_TEST.ps1
+++ METRICAS_PBI/_TEST.ps1
@@ -1,4 +1,8 @@
-$ds = Get-PowerBIDatasource -DatasetId "7d4c845b-e304-42b0-b2af-a1fe14b0db2b"
-write-host $ds
+$applicationId = "035664ed-e0a2-43b2-aba8-c4082cf74a8d";
+$pass = "m7_uViVNo7EU.SA5HN-gS5rMYhDZo490QG"
+$tenant = "a054342c-6f8c-4378-ad61-a8ad00f2b736"
+$securePassword = $pass | ConvertTo-SecureString -AsPlainText -Force
+#$credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $applicationId, $securePassword
+#$x = Connect-PowerBIServiceAccount -ServicePrincipal -Credential $credential -TenantId $tenant
 
-
+$x = Connect-DataGatewayServiceAccount -ApplicationId $applicationId -ClientSecret $securePassword -Tenant $tenant
