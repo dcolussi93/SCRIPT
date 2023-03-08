9311d8ca1673f55cd28235757bc0c70120f1e065
COLUSSI Diego M. TENARIS <DCOLUSSI@tenaris.com>
2023-03-08 11:21:57 -0300

    modif

--- METRICAS_PBI/DATASOURCE/1Admin.ps1
+++ METRICAS_PBI/DATASOURCE/1Admin.ps1
@@ -13,9 +13,9 @@ $f_inicio = (Get-Date -Format "yyyy-MM-dd")+"T00:30:00"
 $f_fin= (Get-Date -Format "yyyy-MM-dd")+"T23:59:00"
 $fech = (Get-Date -Format "yyyyMMdd")
 
-#$f_inicio = "2023-02-17T00:30:00"
-#$f_fin= "2023-02-17T23:59:00"
-#$fech = "20230217"
+#$f_inicio = "2023-02-27T00:30:00"
+#$f_fin= "2023-02-27T23:59:00"
+#$fech = "20230227"
 
 $events = Get-PowerBIActivityEvent -StartDateTime $f_inicio -EndDateTime $f_fin
 $events = ConvertFrom-Json $events
